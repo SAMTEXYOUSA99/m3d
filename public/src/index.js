e35d881325757804e99afc4cc66633c95e26a6e1
SAMTEXYOUSA99 <youcompanysa@gmail.com>
2024-08-03 11:59:31 -0300

    melhor

--- public/src/index.js
+++ public/src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import App from './App';
+//import "slick-carousel/slick/slick.css"; 
+//import "slick-carousel/slick/slick-theme.css";
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log(`API pronta pra uso na porta: ${port}`)
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+app.get('/', (req, res, next) =>{
+  res.json({title: "api em node.js"});
+});
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);*/
 
 
