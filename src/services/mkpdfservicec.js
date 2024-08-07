47279a455f67a7790d41385dfab78ca575630a65
SAMTEXYOUSA99 <youcompanysa@gmail.com>
2024-08-07 12:43:51 -0300

    alt pupp

--- src/services/mkpdfservicec.js
+++ src/services/mkpdfservicec.js
@@ -5,7 +5,9 @@ const base64Img = require('base64-img');
 
 async function generatePDF(mvpproposal) {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+          });
         const page = await browser.newPage();
 
         // Caminho de páginas
